2e147e3fdaf7af58547fbe9e556c666c01f433f5
Otavio Andrade <56943487+otavio-code@users.noreply.github.com>
2025-01-25 16:03:09 -0300

    [FIX] - Ajuste log

--- app/src/service/send_email_notification.py
+++ app/src/service/send_email_notification.py
@@ -5,6 +5,7 @@ from app.src.config.config import get_env_variable
 
 def send_email_notification(to_email, error_message):
     ses_client = boto3.client('ses')
+    logger.info(f"E-mail do cliente: {to_email}")
     response = ses_client.send_email(
         Source=get_env_variable('SES_SOURCE_EMAIL'),
         logger.info("E-mail do cliente: " + to_email)
